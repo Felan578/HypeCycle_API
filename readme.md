ee4b1d8c419f6b74f8e212a578f13a52e60b7c3e
rainatam <rainatam9784@gmail.com>
2019-04-10 17:36:28 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,79 @@
+## Hype Cycle 数据 API
+
+API 提供五种数据，分别是：
+
+- 标准 Hype Cycle 座标
+- 每个技术的论文数序列，包括原始数据和拟合曲线
+- 每个技术的专利序列，包括原始数据和拟合曲线
+- 每个技术的 Hype Cycle
+- (技术,时间) 在标准 Hype Cycle 的座标
+
+下面对每个方法详细说明。
+
+
+
+#### 标准 Hype Cycle 座标
+
+![std_hype_cycle](/Users/Raina/Documents/Study/year three/Lab/GradDesign/model/API/images/std_hype_cycle.png)
+
+用于画出界面中唯一的 Hype Cycle 曲线。
+
+请求方法：`/std_hype_cycle`
+
+返回结果：一个 Dict，分别是 Hype Cycle 四个阶段的座标列表
+
+緃座标 [0,1] ，横座标 [0, 55]，横座标可以不显示。
+
+
+
+#### 论文序列
+
+![papers](/Users/Raina/Documents/Study/year three/Lab/GradDesign/model/API/images/papers.png)请求方法：`/papers/<term>`
+
+返回结果：一个 Dict，给定技术的论文数量原始数据和拟合曲线的座标。
+
+緃座标 [0,1] ，横座标 [1960, 2015]。
+
+
+
+#### 专利序列
+
+![patents](/Users/Raina/Documents/Study/year three/Lab/GradDesign/model/API/images/patents.png)
+
+请求方法：`/patents/<term>`
+
+返回结果：一个 Dict，给定技术的专利数量原始数据和拟合曲线的座标。
+
+緃座标 [0,1] ，横座标 [1960, 2015]。
+
+
+
+#### 技术的 Hype Cycle
+
+![hype_cycle](/Users/Raina/Documents/Study/year three/Lab/GradDesign/model/API/images/hype_cycle.png)
+
+请求方法：`/hype_cycles/<term>`
+
+返回结果：一个 Dict，分别是 Hype Cycle 四个阶段（若存在）的座标列表。
+
+緃座标 [0,1] ，横座标 [1960, 2015]。
+
+
+
+#### (技术,时间) 在标准 Hype Cycle 的座标
+
+![position](/Users/Raina/Documents/Study/year three/Lab/GradDesign/model/API/images/position.png)
+
+用于当用户选择技术和时间后，画出界面中唯一的 Hype Cycle 曲线。
+
+请求方法：`/hype_cycles/<term>/<year>`
+
+返回结果：给定技术和时间在标准 Hype Cycle 的座标。pos 和 value 都是和标准 Hype Cycle 里数值一致的。
+
+```json
+{
+  "pos": 16.2, 
+  "value": 0.9813471542086125
+}
+```
+
---
+++ server.py
@@ -0,0 +1,83 @@
+#!flask/bin/python
+import json
+from flask import Flask, jsonify
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+with open('../result/api_data.json', 'r') as f:
+	data = json.load(f)
+
+class StdHypeCycle(Resource):
+	def __init__(self):
+		self.parser = reqparse.RequestParser()
+		self.std_hype_cycle = data['Std_Hype_cycle']
+
+	def get(self):
+		return jsonify( self.std_hype_cycle )
+
+class HypeCycle(Resource):
+	def __init__(self):
+		self.parser = reqparse.RequestParser()
+		self.hype_cycles = data['Hype_cycles']
+
+	def get(self, term):
+		if term not in self.hype_cycles:
+			abort(404)
+		else:
+			res = self.hype_cycles[term]
+			res['term'] = term
+			return jsonify( res )
+
+class Position(Resource):
+	def __init__(self):
+		self.parser = reqparse.RequestParser()
+		self.parser.add_argument('term', type=str)
+		self.parser.add_argument('year', type=str)
+		self.positions = data['Positions']
+		self.values = data['Std_Hype_cycle_value']
+
+	def get(self, term, year):
+		if term not in self.positions or year not in self.positions[term]:
+			abort(404)
+		else:
+			pos = int(self.positions[term][year])
+			return jsonify( {'pos': pos * 0.1, 'value': self.values[pos]} )
+
+class Patents(Resource):
+	def __init__(self):
+		self.parser = reqparse.RequestParser()
+		self.parser.add_argument('term', type=str)
+		self.patents = data['Patents']
+
+	def get(self, term):
+		if term not in self.patents:
+			abort(404)
+		else:
+			res = self.patents[term]
+			res['term'] = term
+			return jsonify( res )
+
+class Papers(Resource):
+	def __init__(self):
+		self.parser = reqparse.RequestParser()
+		self.parser.add_argument('term', type=str)
+		self.papers = data['Papers']
+
+	def get(self, term):
+		if term not in self.papers:
+			abort(404)
+		else:
+			res = self.papers[term]
+			res['term'] = term
+			return jsonify( res )
+
+api.add_resource(Position, '/position/<term>/<year>')
+api.add_resource(StdHypeCycle, '/std_hype_cycle')
+api.add_resource(HypeCycle, '/hype_cycles/<term>')
+api.add_resource(Patents, '/patents/<term>')
+api.add_resource(Papers, '/papers/<term>')
+
+if __name__ == '__main__':
+	app.run(debug=True)
